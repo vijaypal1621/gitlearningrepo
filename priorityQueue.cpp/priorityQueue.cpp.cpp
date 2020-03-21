a35d8be5c466b143f2e4dce5123ddeef8c5a8d89
unknown <vijay.nsut@gmail.com>
2020-03-21 11:31:26 +0530

    removemin function is introduced

--- priorityQueue.cpp/priorityQueue.cpp.cpp
+++ priorityQueue.cpp/priorityQueue.cpp.cpp
@@ -31,9 +31,46 @@ public:
 			pi = (ci - 1) / 2;
 
 		}
+	}
 
+	int removeMin()
+	{
+		if (pq.size() != 0)
+		{
+			int n = pq.size() - 1;
+			int ans = pq[0];
+			swap(pq[0], pq[n]);
+			
+			//deletion 
+			pq.pop_back();
+			n--; // just to use it furthur
+			int pi = 0;
+			int c1 = 2 * pi + 1;
+			int c2 = 2 * pi + 2;
+			while (c1 <= n)
+			{
+				if (pq[pi] > pq[c2] && pq[c2] < pq[c1] && c2 <= n)
+				{
+					swap(pq[pi], pq[c2]);
+					pi = c2;
+				}
+				else if (pq[pi] > pq[c1])
+				{
+					swap(pq[pi], pq[c1]);
+					pi = c1;
+				}
+				else
+					break;
 
+				c1 = 2 * pi + 1;
+				c2 = 2 * pi + 2;
+			}
 
+			return ans;
+
+		}
+		else
+			return -1;
 	}
 
 
